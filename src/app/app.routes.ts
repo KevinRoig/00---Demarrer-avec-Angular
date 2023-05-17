20b6bc145b06b54bc5e11f6140b33d5713ea7dfe
Kevin <kevinroig37@gmail.com>
2023-05-17 10:34:48 +0200

    improving routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { MenuComponent } from './menu/menu.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { BlockComponent } from './block/block.component';
-import { UserComponent } from './user/user.component';
 import { Routes } from '@angular/router';
 
 const ROUTES: Routes = [
-  { path: 'user', component: UserComponent},
-  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'user', component: UserProfileComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'blocks', component: BlockComponent},
